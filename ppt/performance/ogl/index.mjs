50c712960a1e4b28a711072a1cb5da8110f09b81
spiderT <1304126527@qq.com>
2020-09-09 18:02:15 +0800

    update readme

---
+++ ppt/performance/ogl/index.mjs
@@ -0,0 +1,11 @@
+// Core
+export { Geometry } from './core/Geometry.js';
+export { Program } from './core/Program.js';
+export { Renderer } from './core/Renderer.js';
+export { Transform } from './core/Transform.js';
+export { Mesh } from './core/Mesh.js';
+export { Texture } from './core/Texture.js';
+export { RenderTarget } from './core/RenderTarget.js';
+
+
+
